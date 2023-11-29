3e2ea5d6c63ec9d4dd538ea57065c31498f90ccc
Isaac <isaac.emilien@gmail.com>
2023-11-29 14:39:41 +0000

    Refactor better basket changes in to seperate class

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -13,6 +13,17 @@ public class BetterBasket extends Basket implements Serializable
 {
   private static final long serialVersionUID = 1L;
   
-
   // You need to add code here
+  @Override
+  public boolean add( Product pr )
+  {                              
+    for (Product product : this) {
+      if(product.getProductNum().equals(pr.getProductNum())){
+        product.setQuantity(product.getQuantity() + 1);
+        return true;
+      }
+    }
+    
+    return super.add( pr );     // Call add in ArrayList
+  }
 }
